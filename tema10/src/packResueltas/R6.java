103a3986ea8d83caec54e9ff8f340b69b56902e4
olalla103 <olallalnc@gmail.com>
2024-03-05 20:36:06 +0100

    deberes 5 de marzo

---
+++ tema10/src/packResueltas/R6.java
@@ -0,0 +1,47 @@
+package packResueltas;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class R6 {
+
+	public static void main(String[] args) {
+		// VARIABLES
+		BufferedReader lee = null;
+		String linea = "";
+		int n = 0, suma = 0, cont = 0;
+		double media;
+		Scanner sc = null;
+		try {
+			lee = new BufferedReader(new FileReader("Enteros.txt"));
+			linea = lee.readLine();
+			while (linea != null) {
+				sc = new Scanner(linea);
+				while (sc.hasNextInt()) { // mientras que haya un int, se hace
+					n = sc.nextInt();
+					suma += n;
+					cont++;
+				}
+				linea = lee.readLine();
+			}
+
+			media = suma / cont;
+			System.out.println("La suma total de los números es: " + suma + "\nLa media de los números es: " + media);
+			sc.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (linea != null) {
+				try {
+					lee.close();
+				} catch (Exception e2) {
+					System.out.println(e2.getMessage());
+				}
+			}
+		}
+
+	}
+
+}
