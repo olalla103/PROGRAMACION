bb5d00dee7dc9031df8e921c8dc052a907c4c73d
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-03-19 12:43:54 +0100

    19 de marzo

--- tema10/src/packAplicacion/App12.java
+++ tema10/src/packAplicacion/App12.java
@@ -1,37 +1,37 @@
 package packAplicacion;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String linea = "";
 		Scanner sc;
+		String[] aux;
 		double estatura = 0.0, peso = 0.0;
 		int edad = 0, cont = 0;
 
-		try (BufferedReader lee = new BufferedReader(new FileReader("deportistas.txt"))) {
-			linea = lee.readLine();
-			while (linea != null) {
+		File file = new File("deportistas.txt");
+
+		// Pasar el objeto File al constructor de Scanner
+		try {
+			sc = new Scanner(file);
+			sc.nextLine(); // pasamos primera línea
+			while (sc.hasNextLine()) {
+				aux = sc.nextLine().split("\t+");
+				edad += Integer.parseInt(aux[1]);
+				peso += Double.parseDouble(aux[2]);
+				estatura += Double.parseDouble(aux[3]);
 				cont++;
-				sc = new Scanner(linea);
-				if (sc.hasNextInt()) {
-					edad += sc.nextInt();
-				}
-				if (sc.hasNextDouble()) {
-					peso += sc.nextDouble();
-					estatura += sc.nextDouble();
-				}
-				linea = lee.readLine();
+
 			}
 			System.out.println("La edad media es: " + edad / cont + "\nEl peso medio es: " + peso / cont
-					+ "\nLa estatura media: " + estatura / cont);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
+					+ "\nLa estatura media es: " + estatura / cont);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
