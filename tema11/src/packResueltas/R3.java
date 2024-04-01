05a9c0c98327817317f40ae0e1ea192fb96a7e2a
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-04-01 10:15:49 +0200

    actividades 1 de abril

---
+++ tema11/src/packResueltas/R3.java
@@ -0,0 +1,41 @@
+package packResueltas;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class R3 {
+
+	public static void main(String[] args) {
+		// VARIABLES
+		Scanner sc = new Scanner(System.in);
+		int n, cont = 0;
+		double num;
+
+		System.out.println("Introduce la longitud de la tabla:");
+		n = sc.nextInt();
+
+		// creamos la tabla con n longitud
+		double[] tabla = new double[n];
+
+		// insertamos valores en la tabla
+		do {
+			System.out.println("Introduce un valor (double):");
+			num = sc.nextDouble();
+			tabla[cont] = num;
+			cont++;
+		} while (cont < tabla.length);
+
+		// escribimos en el fichero binario
+		try (ObjectOutputStream escribe = new ObjectOutputStream(new FileOutputStream("resuelta11_3.dat"))) {
+			escribe.writeObject(tabla);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		sc.close();
+
+	}
+
+}
