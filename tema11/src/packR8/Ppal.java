ae89c2bee46b6a1e60eeb8674bf379575f948f5a
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-04-02 12:43:43 +0200

    2 de abril

---
+++ tema11/src/packR8/Ppal.java
@@ -0,0 +1,11 @@
+package packR8;
+
+import java.util.Arrays;
+
+public class Ppal {
+	public static void main(String[] args) {
+		Socio[] tabla = new Socio[0];
+		System.out.println(Arrays.toString(tabla));
+
+	}
+}
---
+++ tema11/src/packR8/Socio.java
@@ -0,0 +1,27 @@
+package packR8;
+
+public class Socio {
+	String dni, nombre;
+
+	public Socio(String dni, String nombre) {
+		this.dni = dni;
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
