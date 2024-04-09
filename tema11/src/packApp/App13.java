8b4ba5f9c620d2b501ad29633340d4d45dff9dfa
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-04-09 12:41:12 +0200

    9 de abril

---
+++ tema11/src/packApp/App13.java
@@ -0,0 +1,45 @@
+package packApp;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class App13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] tabla;
+		tabla = fusionar(null, null);
+		System.out.println(Arrays.toString(tabla));
+
+	}
+
+	public static Integer[] fusionar(String fichero1, String fichero2) {
+		Integer[] tabla = new Integer[0], t1, t2;
+
+		try (ObjectInputStream lee = new ObjectInputStream(new FileInputStream(fichero1 + ".dat"));
+				ObjectInputStream lee2 = new ObjectInputStream(new FileInputStream(fichero2 + ".dat"))) {
+			t1 = (Integer[]) lee.readObject();
+			t2 = (Integer[]) lee2.readObject();
+
+			tabla = new Integer[t1.length + t2.length];
+
+			for (int i = 0; i < t1.length; i++) {
+				tabla[i] = t1[i];
+			}
+			for (int i = t1.length; i < tabla.length; i++) {
+				tabla[i] = t2[i];
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("La clase no corresponde");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return tabla;
+	}
+
+}
