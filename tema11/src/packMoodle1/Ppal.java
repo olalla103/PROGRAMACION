72f6ad79544bdb093f965762fa95c6f1f90f7178
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-04-22 10:26:45 +0200

    22 abril

---
+++ tema11/src/packMoodle1/Ppal.java
@@ -0,0 +1,26 @@
+package packMoodle1;
+
+public class Ppal {
+
+	public static void main(String[] args) {
+		Asignatura prog = new Asignatura("Programación");
+		Asignatura BBDD = new Asignatura("Base de datos");
+		Asignatura sistemas = new Asignatura("Sistemas informáticos");
+		Asignatura lenguaje = new Asignatura("Lenguaje de marcas");
+		Asignatura fol = new Asignatura("FOL");
+		Asignatura entornos = new Asignatura("Entornos de desarrollo");
+		Asignatura moviles = new Asignatura("Móviles");
+		Asignatura[] notasO = { prog, BBDD, sistemas };
+		Asignatura[] notasC = { prog, sistemas };
+		Asignatura[] notasP = { prog, BBDD, sistemas, moviles };
+		Asignatura[] notasI = { prog, BBDD, sistemas };
+
+//		Alumno olalla = new Alumno("Olalla", "78104491X", notasO);
+//		Alumno carmen = new Alumno("Carmen", "1111111C", notasC);
+//		Alumno pablo = new Alumno("Pablo", "22222222P", notasP);
+//		Alumno ines = new Alumno("Ines", "33333333P", notasI);
+
+		Integer sus, aprb, bien, not, sobr;
+	}
+
+}
