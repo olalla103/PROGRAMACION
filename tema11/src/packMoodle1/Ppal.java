78ae014dc3da989db9244729ae930db189559e2f
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-04-24 09:27:51 +0200

    24 de abril

--- tema11/src/packMoodle1/Ppal.java
+++ tema11/src/packMoodle1/Ppal.java
@@ -14,13 +14,14 @@ public class Ppal {
 		Asignatura[] notasC = { prog, sistemas };
 		Asignatura[] notasP = { prog, BBDD, sistemas, moviles };
 		Asignatura[] notasI = { prog, BBDD, sistemas };
+		Integer sus, aprb, bien, not, sobr;
 
-//		Alumno olalla = new Alumno("Olalla", "78104491X", notasO);
+//		Alumno olalla = new Alumno("Olalla", "781h04491X", notasO);
 //		Alumno carmen = new Alumno("Carmen", "1111111C", notasC);
 //		Alumno pablo = new Alumno("Pablo", "22222222P", notasP);
 //		Alumno ines = new Alumno("Ines", "33333333P", notasI);
 
-		Integer sus, aprb, bien, not, sobr;
+	
 	}
 
 }
