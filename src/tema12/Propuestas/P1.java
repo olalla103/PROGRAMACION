6e56a82eadbdb856a43e0ae6c737c811d144cf42
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-05-03 00:19:46 +0200

    3 de mayo

---
+++ src/tema12/Propuestas/P1.java
@@ -0,0 +1,26 @@
+package tema12.Propuestas;
+
+import java.util.Arrays;
+
+public class P1<E> {
+    public static void main(String[] args) {
+        Integer[] tablaN = {1, 2, 3, 4};
+        Integer[] tablaL = {6, 7, 8, 9};
+        Integer[] tabla = juntaTablas(tablaL, tablaN);
+        System.out.println(Arrays.toString(tabla));
+    }
+
+    public static <E> E[] juntaTablas(E[] t1, E[] t2) {
+        E[] tablaFinal = Arrays.copyOf(t1, t1.length + t2.length);
+        for (int i = 0; i < t1.length - 1; i++) {
+            tablaFinal[i] = t1[i];
+        }
+
+        for (int i = 0; i < t2.length; i++) {
+            tablaFinal[t1.length + i] = t2[i];
+        }
+
+
+        return tablaFinal;
+    }
+}
--- src/tema12/Resueltas/R1.java
+++ src/tema12/Resueltas/R1.java
@@ -1,4 +1,20 @@
 package tema12.Resueltas;
 
+import java.util.Arrays;
+
 public class R1 {
+    public static void main(String[] args) {
+        String[] cadenas = {};
+        System.out.println(Arrays.toString(cadenas));
+        cadenas = aniadir("Hola", cadenas);
+        cadenas = aniadir("Adiós", cadenas);
+        System.out.println(Arrays.toString(cadenas));
+
+    }
+
+    public static <E> E[] aniadir(E elemento, E[] tabla) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        tabla[tabla.length - 1] = elemento;
+        return tabla;
+    }
 }
